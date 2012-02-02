6ae905574cea5cac8b4098cf2bef0d0ee957244c
webdos <webdos@webdos-laptop.(none)>
2012-02-02 20:47:59 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '60211d709f51b184a8968ab88e4c84ba1f2a2c1301251351efd3dc8fb2d819f70d8ae53a07f1e0e173f6999eca1d996abeef3b14236a60953272225ff381e892'
